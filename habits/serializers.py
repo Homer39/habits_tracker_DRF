4b0c189042b1b7119de6b3c047ca937e500ff999
vlad <sapozhnikov.061098@mail.ru>
2023-09-25 12:48:39 +0300

    Добавлены валидаторы для привычек

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,6 +1,8 @@
 from habits.models import Habit
 from rest_framework import serializers
 
+from habits.validators import HabitLengthValidator, NiceHabitValidator, LinkedAndRewardValidator, LinkedIsNiceValidator
+
 
 class HabitSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Habit"""
@@ -9,5 +11,6 @@ class HabitSerializer(serializers.ModelSerializer):
         model = Habit
         fields = "__all__"
         validators = [
-
+            LinkedAndRewardValidator, HabitLengthValidator, LinkedIsNiceValidator,
+            NiceHabitValidator
         ]
